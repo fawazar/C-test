c322f7d99f92580b6e2a0c0121750b9144c10cff
fawaz <fawazcode@gmail.com>
2018-03-24 18:02:52 +0300

    test.c First commit

---
+++ test.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+
+
+int a = 10;
+int b = 100;
+
+
+int i,j;
+int arr[2][3] = {{1,2,3},{4,5,6}};
+//addr = &arr;
+
+void myfucntion(int param)
+{
+  for(i = 0;i<2;i++)
+  {
+    for(j=0;j<3;j++)
+    {
+      param[i][j] = 100;
+    }
+  }
+}
+
+
+int main(void)
+{
+
+  printf("%d\n",arr[0][0]);
+
+}
